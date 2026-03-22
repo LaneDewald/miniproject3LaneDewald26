774a5c1b768f81383ab77fb5c4815a76d6f1fef7
Lane <l_dewald3@mail.fhsu.edu>
2026-03-22 13:42:53 -0500

    Created database modules

--- main.py
+++ main.py
@@ -14,3 +14,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///watchlist.db'
 
 db = SQLAlchemy(app)
 
+
+# User table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    movies = db.relationship('Movie', backref='owner', lazy=True)
+
+
+# Movie table
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    genre = db.Column(db.String(80))
+    watched = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+
